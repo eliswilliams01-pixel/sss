8bb09c50fddeda9babebe6b4a4ae0a1edb20ef38
eliswilliams01-pixel <eliswilliams01@gmail.com>
2026-06-02 15:04:25 +0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
 set -e
 
-# Обязательные переменные
-: "${BACKEND_HOST:?Переменная BACKEND_HOST не задана}"
-: "${BACKEND_PORT:?Переменная BACKEND_PORT не задана}"
-
-# Опциональные — с дефолтами
-LISTEN_PORT="${LISTEN_PORT:-8443}"
-WS_PATH="${WS_PATH:-/ws}"
-GRPC_PATH="${GRPC_PATH:-/grpc}"
-XHTTP_PATH="${XHTTP_PATH:-/xhttp}"
-ORIGIN_URL="${ORIGIN_URL:-https://www.google.com}"
-
-export LISTEN_PORT WS_PATH GRPC_PATH XHTTP_PATH ORIGIN_URL
-
 echo "Starting nginx:"
-echo "  LISTEN_PORT  = $LISTEN_PORT"
-echo "  BACKEND_HOST = $BACKEND_HOST"
-echo "  BACKEND_PORT = $BACKEND_PORT"
-echo "  WS_PATH      = $WS_PATH"
-echo "  GRPC_PATH    = $GRPC_PATH"
-echo "  XHTTP_PATH   = $XHTTP_PATH"
-echo "  ORIGIN_URL   = $ORIGIN_URL"
-
-envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${LISTEN_PORT} ${WS_PATH} ${GRPC_PATH} ${XHTTP_PATH} ${ORIGIN_URL}' \
-    < /etc/nginx/nginx.conf.template \
-    > /etc/nginx/nginx.conf
 
 nginx -t
 
